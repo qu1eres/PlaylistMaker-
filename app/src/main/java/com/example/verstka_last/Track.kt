8a57c22d01cf2332946bbc0b3bb1262c941bc2a8
quieres <qu1eres@yandex.ru>
2025-08-20 20:12:35 +0300

    11 sprint

--- app/src/main/java/com/example/verstka_last/Track.kt
+++ app/src/main/java/com/example/verstka_last/Track.kt
@@ -1,4 +1,24 @@
 package com.example.verstka_last
 
-class Track(val trackName: String, val artistName: String, val trackTime: String, val artworkUrl100: String) {
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+data class Track(
+    val title: String,
+    val artist: String,
+    val duration: String,
+    val artworkUrl: String
+)
+
+fun ITunesTrack.toTrack(): Track {
+    val duration = trackTimeMillis?.let {
+        SimpleDateFormat("mm:ss", Locale.getDefault()).format(it)
+    } ?: "00:00"
+
+    return Track(
+        title = trackName ?: "",
+        artist = artistName ?: "",
+        duration = duration,
+        artworkUrl = artworkUrl100 ?: ""
+    )
 }
