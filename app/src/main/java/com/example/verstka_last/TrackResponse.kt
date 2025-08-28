f07b651cf0a6f70766542e5c9b51ebd4b9780e1b
quieres <qu1eres@yandex.ru>
2025-08-28 09:03:41 +0300

    13 sprint

--- app/src/main/java/com/example/verstka_last/TrackResponse.kt
+++ app/src/main/java/com/example/verstka_last/TrackResponse.kt
@@ -10,5 +10,9 @@ data class ITunesTrack(
     val trackName: String?,
     val artistName: String?,
     val trackTimeMillis: Long?,
-    val artworkUrl100: String?
+    val artworkUrl100: String?,
+    val collectionName: String?,
+    val releaseDate: String?,
+    val primaryGenreName: String?,
+    val country: String?
 )
