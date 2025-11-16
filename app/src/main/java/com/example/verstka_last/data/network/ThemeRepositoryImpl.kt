e2e940679a63826c8d0fe7c71560dbfa057b7d47
quieres <qu1eres@yandex.ru>
2025-11-16 16:09:49 +0300

    15 part 4 final (hopefully)

---
+++ app/src/main/java/com/example/verstka_last/data/network/ThemeRepositoryImpl.kt
@@ -0,0 +1,4 @@
+package com.example.verstka_last.data.network
+
+class ThemeRepositoryImpl {
+}
---
+++ app/src/main/java/com/example/verstka_last/domain/api/SearchHistoryRepository.kt
@@ -0,0 +1,4 @@
+package com.example.verstka_last.domain.api
+
+interface SearchHistoryRepository {
+}
