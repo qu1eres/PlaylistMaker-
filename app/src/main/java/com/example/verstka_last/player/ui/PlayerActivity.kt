2094fb50f1c88c4364465f0c0dffadc9958023e0
quieres <qu1eres@yandex.ru>
2025-11-21 23:03:54 +0300

    16 sprint trouble

--- app/src/main/java/com/example/verstka_last/player/ui/PlayerActivity.kt
+++ app/src/main/java/com/example/verstka_last/player/ui/PlayerActivity.kt
@@ -1,20 +1,15 @@
 package com.example.verstka_last.player.ui
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.verstka_last.R
 import com.example.verstka_last.creator.Creator
 import com.example.verstka_last.core.domain.models.Track
 import com.example.verstka_last.databinding.ActivityAudioplayerBinding
-import com.google.gson.Gson
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 class PlayerActivity : AppCompatActivity() {
 
@@ -26,8 +21,11 @@ class PlayerActivity : AppCompatActivity() {
         binding = ActivityAudioplayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val trackJson = intent.getStringExtra(TRACK_KEY)
-        val track = Gson().fromJson(trackJson, Track::class.java)
+        @Suppress("DEPRECATION")
+        val track = intent.getSerializableExtra(TRACK_KEY) as? Track ?: run {
+            finish()
+            return
+        }
 
         val factory = Creator.providePlayerViewModelFactory()
         viewModel = ViewModelProvider(this, factory)[PlayerViewModel::class.java]
@@ -49,7 +47,7 @@ class PlayerActivity : AppCompatActivity() {
         }
 
         viewModel.currentTime.observe(this) { time ->
-            binding.trackDuration.text = time
+            binding.currentPlayTime.text = time
         }
     }
 
@@ -61,7 +59,8 @@ class PlayerActivity : AppCompatActivity() {
     private fun render(state: PlayerState) {
         when (state) {
             PlayerState.Default -> {
-                binding.play.isEnabled = false
+                binding.play.isEnabled = true
+                binding.play.setImageResource(R.drawable.ic_button_play)
             }
             PlayerState.Prepared -> {
                 binding.play.isEnabled = true
@@ -81,12 +80,18 @@ class PlayerActivity : AppCompatActivity() {
     private fun setupUI(track: Track) {
         binding.trackName.text = track.title
         binding.artistName.text = track.artist
-        binding.trackDuration.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(track.duration)
-        binding.albumName.text = track.collectionName
-        binding.year.text = track.getReleaseYear()
+        binding.trackDuration.text = track.duration
+
+        if (track.collectionName != null) {
+            binding.albumName.text = track.collectionName
+            binding.albumName.isVisible = track.collectionName.isNotEmpty()
+        } else {
+            binding.albumName.isVisible = false
+        }
+
+        binding.year.text = track.getReleaseYear() ?: ""
         binding.genreName.text = track.primaryGenreName
         binding.country.text = track.country
-
 
         Glide.with(this)
             .load(track.getHighResArtworkUrl())
