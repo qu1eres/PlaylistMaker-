d8d03a545c4ae5f3c5182ce43e62b7a5f9a3688b
quieres <qu1eres@yandex.ru>
2025-11-24 16:32:26 +0300

    17 sprint я еблан тупой

--- app/src/main/java/com/example/verstka_last/di/PlayerModule.kt
+++ app/src/main/java/com/example/verstka_last/di/PlayerModule.kt
@@ -1,5 +1,6 @@
 package com.example.verstka_last.di
 
+import android.media.MediaPlayer
 import com.example.verstka_last.player.data.impl.PlayerRepositoryImpl
 import com.example.verstka_last.player.domain.api.PlayerInteractor
 import com.example.verstka_last.player.domain.api.PlayerRepository
@@ -9,7 +10,7 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val playerModule = module {
-    factory<PlayerRepository> { PlayerRepositoryImpl() }
+    factory<PlayerRepository> { PlayerRepositoryImpl { MediaPlayer() } }
     factory<PlayerInteractor> { PlayerInteractorImpl(get()) }
     viewModel { PlayerViewModel(get()) }
 }
