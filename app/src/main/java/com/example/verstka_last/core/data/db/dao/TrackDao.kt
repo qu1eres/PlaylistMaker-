dbe4f90592b8ca0da91618e5b9eadd0fb60af060
qu1eres <qu1eres@yandex.ru>
2026-02-02 04:48:22 +0300

    Обновляемся во всех смыслах и небольшой фикс

--- app/src/main/java/com/example/verstka_last/core/data/db/dao/TrackDao.kt
+++ app/src/main/java/com/example/verstka_last/core/data/db/dao/TrackDao.kt
@@ -1,11 +1,13 @@
 package com.example.verstka_last.core.data.db.dao
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.verstka_last.core.data.db.entity.TrackEntity
 
+@Dao
 interface TrackDao {
     @Insert(entity = TrackEntity::class, onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTracks(track: TrackEntity)
