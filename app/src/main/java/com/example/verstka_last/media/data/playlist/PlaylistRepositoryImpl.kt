bcbabaa291d65bcaeb09db44513d8724245c5821
qu1eres <qu1eres@yandex.ru>
2026-02-08 15:14:32 +0300

    .

--- app/src/main/java/com/example/verstka_last/media/data/playlist/PlaylistRepositoryImpl.kt
+++ app/src/main/java/com/example/verstka_last/media/data/playlist/PlaylistRepositoryImpl.kt
@@ -4,7 +4,6 @@ import com.example.verstka_last.core.data.converters.PlaylistDbConverter
 import com.example.verstka_last.core.data.db.AppDatabase
 import com.example.verstka_last.core.data.db.JsonMapper
 import com.example.verstka_last.core.data.db.entity.PlaylistEntity
-import com.example.verstka_last.core.data.db.entity.PlaylistTrackEntity
 import com.example.verstka_last.core.domain.models.Playlist
 import com.example.verstka_last.core.domain.models.Track
 import com.example.verstka_last.media.domain.playlist.PlaylistRepository
@@ -22,49 +21,10 @@ class PlaylistRepositoryImpl(private val appDatabase: AppDatabase, private val p
     }
 
     override suspend fun addTrack(track: Track, playlist: Playlist) {
-        val playlistTrackEntity = convertTrackToPlaylistTrackEntity(track)
-        appDatabase.playlistTrackDao().insertTrack(playlistTrackEntity)
-
-        val currentTracks = jsonMapper.convertToList(playlist.tracks)
-
-        if (currentTracks.any { it.trackId == track.trackId }) {
-            return
-        }
-
-        val updatedTracks = currentTracks.toMutableList().apply {
-            add(0, track)
-        }
-        playlist.tracks = jsonMapper.write(updatedTracks)
-        playlist.trackCount = updatedTracks.size.toLong()
-
+        ++playlist.trackCount
+        playlist.tracks = jsonMapper.setTrack(track, playlist.tracks)
         val playlistEntity = convertFromPlaylist(playlist)
         appDatabase.playlistDao().updatePlaylist(playlistEntity)
-    }
-
-    private fun convertTrackToPlaylistTrackEntity(track: Track): PlaylistTrackEntity {
-        return PlaylistTrackEntity(
-            trackId = track.trackId.toString(),
-            trackName = track.title,
-            artistName = track.artist,
-            collectionName = track.collectionName,
-            releaseDate = track.releaseDate,
-            primaryGenreName = track.primaryGenreName,
-            country = track.country,
-            previewUrl = track.previewUrl,
-            artworkUrl100 = track.artworkUrl,
-            trackTimeMillis = track.duration.toLongOrNull()
-        )
-    }
-
-    override suspend fun getTracksForPlaylist(playlistId: Long): List<Track> {
-        val playlist = appDatabase.playlistDao().getPlaylists()
-            .firstOrNull { it.id == playlistId }
-
-        return if (playlist != null && !playlist.tracks.isNullOrEmpty()) {
-            jsonMapper.convertToList(playlist.tracks)
-        } else {
-            emptyList()
-        }
     }
 
     override suspend fun getTrackList(playlist: Playlist): List<Track> {
